25789d58ef6acab5ff3e7be4a1e2c049b4de1f86
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-05-17 10:28:40 +0200

    Added two methods for the Carouosel class / Carousel.js complete

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -7,18 +7,28 @@ class Carousel {
         this.index = 0;
         this.left.addEventListener("click", () => this.leftPicture());
         this.right.addEventListener("click", () => this.rightPicture());
+        this.img[0].style.display = "block";
     }
     leftPicture() {
-
+        this.img[this.index].style.display = "none";
+        this.index += 1;
+        if (this.index > 3) {
+            this.index -= 4;
+        }
+        this.img[this.index].style.display = "block";
     }
 
     rightPicture() {
-        
+        this.img[this.index].style.display = "none";
+        this.index -= 1;
+        if (this.index < 0) {
+            this.index += 4;
+        }
+        this.img[this.index].style.display = "block";
     }
-
 }
 
-let carousel = document.querySelector(".carousel").forEach(el => new Carousel(el));
+let carousel = document.querySelectorAll(".carousel").forEach(el => new Carousel(el));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
